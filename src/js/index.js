6969cfb5de53617fc43c828a7be0a5317a9d7396
ArturBaur <artur.baur@gmail.com>
2023-09-28 22:22:29 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,81 +4,70 @@ import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const catInfo = document.querySelector('.cat-info');
-const breedSelect = document.querySelector('.breed-select');
+document.addEventListener('DOMContentLoaded', async () => {
+  console.log('Document loaded');
+  Notiflix.Loading.standard('Loading data, please wait...', {
+    backgroundColor: 'rgba(0,0,0,0.8)',
+  });
 
-Notiflix.Loading.standard('Loading...', {
-  backgroundColor: 'rgba(0,0,0,0.8)',
-});
-
-window.onload = async () => {
   try {
-    console.log('Window loaded');
-    Notiflix.Loading.standard('Loading data, please wait...', {
-      backgroundColor: 'rgba(0,0,0,0.8)',
-    });
-
     const breeds = await fetchBreeds();
 
     Notiflix.Loading.remove();
+    const breedSelect = document.querySelector('.breed-select');
+    let select;
 
-    initializeBreedSelect(breeds);
+    select = new SlimSelect({
+      select: breedSelect,
+      data: breeds.map(breed => ({
+        text: breed.name,
+        value: breed.id,
+      })),
+    });
+
+    breedSelect.addEventListener('change', event => {
+      console.log('onChange event', event);
+      console.log('onChange event - selected value', event.target.value);
+      displayCatInfo(event.target.value);
+    });
+
+    console.log('SlimSelect initialized: ', select);
+    console.log('SlimSelect data: ', select.data.getData());
   } catch (error) {
     Notiflix.Loading.remove();
-    handleFetchError();
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
   }
-};
 
-function initializeBreedSelect(breeds) {
-  select = new SlimSelect({
-    select: breedSelect,
-    data: breeds.map(breed => ({
-      text: breed.name,
-      value: breed.id,
-    })),
-  });
-
-  breedSelect.addEventListener('change', event => {
-    console.log('onChange event', event);
-    console.log('onChange event - selected value', event.target.value);
-    displayCatInfo(event.target.value);
-  });
-
-  console.log('SlimSelect initialized: ', select);
-  console.log('SlimSelect data: ', select.data.getData());
-}
-
-async function displayCatInfo(breedId) {
-  try {
+  function displayCatInfo(breedId) {
     console.log('About to fetch cat by breed');
     console.log('displayCatInfo', breedId);
     Notiflix.Loading.standard('Loading data, please wait...', {
       backgroundColor: 'rgba(0,0,0,0.8)',
     });
 
-    const cat = await fetchCatByBreed(breedId);
-
-    Notiflix.Loading.remove();
-
-    catInfo.innerHTML = `
-      <img src="${cat.url}" alt="${cat.breeds[0].name}">
-      <div class="description">
-          <h2>${cat.breeds[0].name}</h2>
-          <p>${cat.breeds[0].description}</p>
-          <p>${cat.breeds[0].temperament}</p>
-      </div>
-    `;
-
-    console.log(catInfo);
-  } catch (error) {
-    console.log(error.response);
-    Notiflix.Loading.remove();
-    handleFetchError();
+    fetchCatByBreed(breedId)
+      .then(cat => {
+        console.log('Fetched cat info', cat);
+        Notiflix.Loading.remove();
+        const catInfo = document.querySelector('.cat-info');
+        catInfo.innerHTML = `
+          <img src="${cat.url}" alt="${cat.breeds[0].name}">
+          <div class="description">
+            <h2>${cat.breeds[0].name}</h2>
+            <p>${cat.breeds[0].description}</p>
+            <p>${cat.breeds[0].temperament}</p>
+          </div>
+        `;
+        console.log(catInfo);
+      })
+      .catch(error => {
+        console.log(error.response);
+        Notiflix.Loading.remove();
+        Notiflix.Notify.failure(
+          'Oops! Something went wrong! Try reloading the page!'
+        );
+      });
   }
-}
-
-function handleFetchError() {
-  Notiflix.Notify.failure(
-    'Oops! Something went wrong! Try reloading the page!'
-  );
-}
+});
